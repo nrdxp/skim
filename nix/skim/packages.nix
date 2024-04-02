12de94c42799bd546a45392abc65bf626b911f20
Timothy DeHerrera <tim@nrdxp.dev>
2024-04-02 13:37:54 -0600

    fix(nix): package man page and shell scripts

--- nix/skim/packages.nix
+++ nix/skim/packages.nix
@@ -4,8 +4,25 @@
 }: let
   inherit (inputs.nixpkgs) pkgs;
   inherit (inputs.cells.repo.rust) toolchain;
-  buildRustCrateForPkgs = pkgs: pkgs.buildRustCrate.override { rustc = toolchain; cargo = toolchain;};
+  buildRustCrateForPkgs = pkgs:
+    pkgs.buildRustCrate.override {
+      rustc = toolchain;
+      cargo = toolchain;
+    };
 in {
   # sane default for a binary package
-  default = (pkgs.callPackage "${inputs.self}/Cargo.nix" { inherit buildRustCrateForPkgs pkgs; inherit (inputs) nixpkgs;}).rootCrate.build;
+  default =
+    (pkgs.callPackage "${inputs.self}/Cargo.nix" {
+      inherit buildRustCrateForPkgs pkgs;
+      inherit (inputs) nixpkgs;
+    })
+    .rootCrate
+    .build
+    .overrideAttrs (_: {
+      postInstall = ''
+        mkdir -p $out/share
+        cp -r man $out/share
+        cp -r shell $out/share/skim
+      '';
+    });
 }
